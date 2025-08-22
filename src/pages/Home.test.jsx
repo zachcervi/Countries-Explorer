16a6e83852584a9f2f96d6d279e44c5c4c2fcea7
Zach Cervi <zachcervi@gmail.com>
2025-08-22 09:49:55 -0600

    added tests for header, countries list section, and verify proper html structure -> red

--- src/pages/Home.test.jsx
+++ src/pages/Home.test.jsx
@@ -21,4 +21,20 @@ describe("Home Page", () => {
   it("renders without crashing", () => {
     render(<Home />);
   });
+  it("renders a header section", () => {
+    render(<Home />);
+    const header = screen.getByRole("banner");
+    expect(header).toBeInTheDocument();
+  });
+  it("renders a countries list section", () => {
+    render(<Home />);
+    const countriesSection =
+      ServiceWorkerContainer.getByTestId("countries-section");
+    expect(countriesSection).toBeInTheDocument();
+  });
+  it("has proper HTML structure", () => {
+    render(<Home />);
+    expect(screen.getByRole("main")).toBeInTheDocument();
+    expect(screen.getByRole("banner")).toBeInTheDocument();
+  });
 });
